6e92376b06984a90d1556771c7be047ad991d24c
Small little <submit2mxh@gmail.com>
2025-10-11 08:32:24 +0000

    Try fixing this error: `Console Error: [2025-10-11T08:31:39.316Z] @fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,14 @@
+// 1. Go to your Firebase project console.
+// 2. In Project settings, find your web app.
+// 3. Find and copy the firebaseConfig object.
+// 4. Paste it here.
+
 export const firebaseConfig = {
-  "projectId": "studio-4954880629-a3af8",
-  "appId": "1:420859068847:web:15def5b01930d1eb11ef5a",
-  "apiKey": "AIzaSyCqD9Yhli0SyOSIjmBhbQb3QbWzGcUToWI",
-  "authDomain": "studio-4954880629-a3af8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "420859068847"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID" // Optional
 };
